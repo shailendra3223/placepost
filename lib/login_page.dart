aaddcf05c85d2e44c0d050b7578974a607540008
SHAILENDRA <sam724673@gmail.com>
2022-05-27 17:07:07 +0530

    some changes

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,29 @@
+import 'package:digitalmaster/dashboard_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class LogInPage extends StatelessWidget {
+class LogInPage extends GetWidget {
   const LogInPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body:Column(
+        children: [
+           MaterialButton(
+             onPressed: () {
+               Get.to(()=>
+                   DashBoardPage()
+               );
+
+             },
+            child: Text("Submit"),
+          ),
+        ],
+      ),
+    );
   }
 }
